581150ca595b8b79df307cd208c4d07639181098
Kwonkyu <root@localhost>
2019-03-26 03:10:44 +0900

    Implemented ui.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("OS Process Scheduling");
+        primaryStage.setScene(new Scene(root, 600, 700));
         primaryStage.show();
     }
 
