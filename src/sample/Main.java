ae42cf95d7a5e4da76d7473e09d5d4cf13af17dd
Kwonkyu <root@localhost>
2019-03-30 02:02:45 +0900

    minor design update. - Changed process generation box with Rectangle, not 4 piece of line. - GridPane to user input(radio button, text field...) - Implemented Scheduling Algorithm radio button(multiple selection is available. << should be disabled.) - TableView to Process Information and output table field.
    TODO:
    - Use SplitPane to split input(process generation, information...) and output(gantt chart, output table).
    - Dynamically resizable elements.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("OS Process Scheduling");
-        primaryStage.setScene(new Scene(root, 600, 700));
+        primaryStage.setScene(new Scene(root, 700, 700));
         primaryStage.show();
     }
 
