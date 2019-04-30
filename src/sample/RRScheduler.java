79f31c472312e54eb29deecf8cb5b8be19690648
Overhead1284 <alsrl1284@gmail.com>
2019-04-30 22:35:02 +0900

    ..

--- src/sample/RRScheduler.java
+++ src/sample/RRScheduler.java
@@ -1,5 +1,5 @@
 public class RRScheduler extends Scheduler {
-    private int delta;
+    private int delta; // 최대 실행 시간
 
     public RRScheduler(int _delta){
         setDelta(_delta);
@@ -13,35 +13,49 @@ public class RRScheduler extends Scheduler {
     }
 
     public void changeProcess(int currentTime){
-        runningProcess.setIdleTime(currentTime);
-        result.add(new Process(runningProcess.getID(), runningProcess.getArrivalTime(), runningProcess.getBurstTime(), runningProcess.getIdleTime(), runningProcess.getAwakeTime()));
+        Processor.setIdleTime(currentTime);
+        if(Processor.getArrivalTime() != Processor.getIdleTime()) result.add(new Process(Processor.getID(), Processor.getAwakeTime(), Processor.getIdleTime()));
+        // 프로세스 처리가 다 안됬으면 다시 큐에 삽입
+        if(Processor.getBurstTime() > Processor.getRunningTime()) queue.add(Processor);
+        //대기 큐가 비어있지 않은 경우 프로세스 교체
         if(!queue.isEmpty()) {
-            if(runningProcess.getBurstTime() > runningProcess.getRunningTime()) queue.add(runningProcess); // 프로세스 처리가 다 안됬으면 다시 큐에 삽입
-            runningProcess = queue.get(0);
-            runningProcess.setAwakeTime(currentTime);
+            Processor = queue.get(0);
+            Processor.setAwakeTime(currentTime);
             queue.remove(0);
         }
-        else this.setIdle();
+        //비어있는 경우 프로세서를 idle 상태로 지정
+        else this.setIdle(currentTime);
     }
 
     public void run(){
-        int runoutTimer = 0;
+        int runOutTimer = 0;
         int schedulingTime = getSchedulingTime();
 
         for(int i = 0; i < schedulingTime + 1; i++){
             insertQueue(i);
-            if(runningProcess.getID().equals("idle") && !queue.isEmpty()){
-                runningProcess = queue.get(0);
-                queue.remove(0);
+            if(Processor.getID().equals("idle")){
+                // idle 상태 해제
+                if(!queue.isEmpty()){
+                    // 초기값이 아닌 idle 상태인 경우 스케줄링 시간 추가
+                    if(Processor.getArrivalTime() != 0) schedulingTime++;
+                    changeProcess(i);
+                }
+                // idle 상태 유지
+                else{
+                    schedulingTime++;
+                    continue;
+                }
             }
-            if(runningProcess.getBurstTime() == runningProcess.getRunningTime() || runoutTimer == this.getDelta()){
-                runningProcess.setTurnaroundTime(i - runningProcess.getArrivalTime());
+            // 프로세스 종료 후 또는 런아웃 타이머에 의해 교체
+            if(Processor.getBurstTime() == Processor.getRunningTime() || runOutTimer == this.getDelta()){
+                Processor.setTurnaroundTime(i - Processor.getArrivalTime());
                 changeProcess(i);
-                runoutTimer = 0;
+                runOutTimer = 0;
             }
-            if(!runningProcess.getID().equals("idle")){
-                runningProcess.increasRunningTime();
-                runoutTimer++;
+            // 프로세스의 현재 누적 실행 시간 및 런아웃타이머 증가
+            if(!Processor.getID().equals("idle")){
+                Processor.increasRunningTime();
+                runOutTimer++;
             }
         }
         printResult();
