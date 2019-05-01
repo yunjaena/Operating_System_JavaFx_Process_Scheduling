ad658af485f8cfd22ed6374caf35cdf3ae2e78a9
Na Yunjae_Android <yunjaena@gmail.com>
2019-05-02 01:10:08 +0900

    add gantt chart

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,26 +2,24 @@ package sample;
 
 import javafx.application.Application;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.chart.CategoryAxis;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.StackedBarChart;
-import javafx.scene.chart.XYChart;
+import javafx.scene.chart.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Random;
+import java.util.*;
 
 import sample.FCFSScheduler;
+
 
 public class Main extends Application {
 
@@ -36,12 +34,14 @@ public class Main extends Application {
     private RadioButton SRTN;
     private RadioButton HRRN;
     private RadioButton FTW;
-    private StackedBarChart<Number, String> ganttChart;
+    final NumberAxis xAxis = new NumberAxis();
+    final CategoryAxis yAxis = new CategoryAxis();
+    @FXML
+    private static GanttChart<Number, String> ganttChart;
     private CategoryAxis processList;
     private NumberAxis processTime;
     private Parent root;
-
-    private ArrayList<Process> processArrayList = new ArrayList<>();
+    private static ArrayList<Process> processArrayList = new ArrayList<>();
 
     private int pidSequence = 0;
 
@@ -62,8 +62,6 @@ public class Main extends Application {
         processList.setLabel("category");
         processTime.setLabel("Process scheduling.");
 
-        var processKind = FXCollections.<String>observableArrayList(Arrays.asList("PROCESS"));
-        processList.setCategories(processKind);
         // -------------------------------------------------------------------------
 
         // ------------------------ [ Process Add Button ] -------------------------
@@ -84,7 +82,7 @@ public class Main extends Application {
     }
 
     private void initView() {
-        ganttChart = (StackedBarChart) root.lookup("#gantt_chart");
+        ganttChart = (GanttChart) root.lookup("#gantt_chart");
         processInputTime = (TextField) root.lookup("#input_time");
         processBurstTime = (TextField) root.lookup("#burst_time");
         FCFS = (RadioButton) root.lookup("#FCFS");
@@ -95,24 +93,35 @@ public class Main extends Application {
         FTW = (RadioButton) root.lookup("#FTW");
     }
 
-    private static void fcfs(Scheduler s) {
+    private void activateChart(Scheduler s) {
+        ArrayList<XYChart.Series<Number, String>> schedulings = new ArrayList<>();
+        HashMap<String, String> colorHashMap = new HashMap<>();
+        s.pArr = processArrayList;
+        s.run();
+
+        for (Process process : s.result) {
+            XYChart.Series<Number, String> scheduling = new XYChart.Series<>();
+            scheduling.getData().add(new XYChart.Data<>(process.getArrivalTime(), process.getPID(), new GanttChart.ExtraData(process.getBurstTime() - process.getArrivalTime(), "status-red")));
+            schedulings.add(scheduling);
+        }
+        ganttChart.getStylesheets().add(getClass().getResource("ganttchartStyle.css").toExternalForm());
+        ganttChart.getData().addAll(schedulings);
+
+
+    }
+
+    public void setColor(ArrayList<XYChart.Series<Number, String>> schedulings) {
 
     }
 
     public void onClickedScheduleButton() {
         System.out.println("Process Scheduling Button clicked.");
 
-        ArrayList<XYChart.Series<Number, String>> schedulings = new ArrayList<>();
 
         if (FCFS.isSelected()) {
             System.out.println("FCFS");
-              /*  fcfs(new FCFSScheduler());
-                Scheduler s = new FCFSScheduler();
-                Collections.copy(s.pArr, processArrayList);
-                s.run();
-                for(int i=0;i<s.result.size();i++){
+            activateChart(new FCFSScheduler());
 
-                }*/
         } else if (RR.isSelected()) {
             System.out.println("RR");
         } else if (SPN.isSelected()) {
@@ -126,19 +135,6 @@ public class Main extends Application {
         }
 
 
-        for (int i = 0; i < 3; i++) {
-            XYChart.Series<Number, String> scheduling = new XYChart.Series<>();
-            // XYChart.Data<Number, String> data = new XYChart.Data<>(10, "PROCESS 0");
-            scheduling.getData().add(new XYChart.Data<>(10, ""));
-            schedulings.add(scheduling);
-        }
-
-        //ganttChart.getData().addAll(scheduling1, scheduling2, scheduling3);
-        ganttChart.getData().addAll(schedulings);
-
-        for (Node n : ganttChart.lookupAll(".default-color0.chart-bar")) {
-            n.setStyle("-fx-bar-fill: yellow;");
-        }
     }
 
     public void onClickedProccessAddButton() {
@@ -161,8 +157,7 @@ public class Main extends Application {
         // get TableView and insert Process. Add to arraylist too.
     }
 
-    public void fcfsGanttChart()
-    {
+    public void fcfsGanttChart() {
 
     }
 
